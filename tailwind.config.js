ef1e5a4e2a6fab5059c0c8f1d6b16483c9539769
Uefa Yudsa-ad <145674290+Hyke02@users.noreply.github.com>
2025-03-13 14:18:08 +0100

    config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,28 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src//*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         'background' : '#EFF7FF',
-        'button' : '#CCE6FE'
-      },
-      fontFamily: {
-        kulim: ["Kulim Park", "sans-serif"]
-      },
-      backgroundImage: {
-        'blue-white-gradient': "url('src/assets/background/mesh-gradient-blue-white.png')"
-      }
-    },
         'button' : '#CCE6FE',
         'headerBlue' : '#6494ED',
         'correct' : '#73FFE8',
         'incorrect' : '#FF1A7D',
         'offwhite' : '#F8F7F7'
       },
+      backgroundImage: {
+        'blue-white-gradient': "url('src/assets/background/mesh-gradient-blue-white.png')"
+      },
       fontFamily: {
         kulim: ["Kulim Park", "sans-serif"]
-      }
-    },
+      },
+    }
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
   ],
 }
-
